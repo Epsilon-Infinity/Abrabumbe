21b70833257c59369488f8ee2e94fadd4ec61a62
Kolawole <bamtak@hotmail.com>
2020-02-20 18:09:44 +0000

    add Library

---
+++ model.py
@@ -0,0 +1,7 @@
+
+class Library:
+    def __init__(self, books, signup_days, book_p_day):
+        self.books = books
+        self.signup_days = signup_days
+        self.book_p_day = book_p_day
+        
