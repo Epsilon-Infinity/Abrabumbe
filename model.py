4ff1b8d2bb2b7efaae67dc1182db9276232ac9ff
Kolawole <bamtak@hotmail.com>
2020-02-20 18:43:20 +0000

    add score

--- model.py
+++ model.py
@@ -1,7 +1,14 @@
 
 class Library:
+
     def __init__(self, books, signup_days, book_p_day):
         self.books = books
         self.signup_days = signup_days
         self.book_p_day = book_p_day
+
+    def score(self):
+        return (sum(self.books.values()) + self.book_p_day) / self.signup_days
+
+    def score2(self):
+        pass
         
